518c603dd41238ea6b586305c835c2c292fc869c
Ivan Khokhlov <lostpointmeister@gmail.com>
2025-07-24 13:23:17 +0000

    ttt

--- task_03/src/main.cpp
+++ task_03/src/main.cpp
@@ -1,15 +1,3 @@
 #include <iostream>
 
-struct VAL {
-  struct Reg01 {
-    enum B {
-      first,
-    }
-  };
-};
-
-class error : public std::exception {
-  using std::exception::exception;
-};
-
 int main() { return 0; }
